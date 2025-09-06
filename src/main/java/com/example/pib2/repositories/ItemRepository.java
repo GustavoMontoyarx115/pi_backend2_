88d7da832ec103bbd31ede453c2e5e34bd0f5c09
unknown <ta.vorx115@gmail.com>
2025-09-06 15:44:24 -0500

    se cambia la informacion del json por la del lider

--- src/main/java/com/example/pib2/repositories/ItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.pib2.repositories;
-
-import com.example.pib2.models.entities.Item;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ItemRepository extends JpaRepository<Item, Long> {
-}
--- src/main/java/com/example/pib2/repositories/LoanRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.pib2.repositories;
-
-import com.example.pib2.models.entities.Loan;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LoanRepository extends JpaRepository<Loan, Long> {
-}
