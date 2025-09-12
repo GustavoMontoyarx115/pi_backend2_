77f82e6f35a7b88115699ef38a26c2bf48755bd6
unknown <ta.vorx115@gmail.com>
2025-09-12 18:38:11 -0500

    Eliminados archivos obsoletos

--- src/main/java/com/example/pib2/repositories/AppointmentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.pib2.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.pib2.models.entities.Appointment;
-
-public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
-    // Aquí puedes definir consultas personalizadas si las necesitas
-}
